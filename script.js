499d3424f9b4db2ecf8fbd8097e95761884c8ac9
tmcode7 <mawuenatord7@gmail.com>
2022-01-02 21:32:59 +0000

    corrected data-key values which were causing a mixup in the sound output

--- script.js
+++
@@ -1,5 +0,0 @@
-//set up the event listener as step 1
-window.addEventListener('keydown', function(e,keyCode){
- const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
- console.log(audio)
-})
